0547a6c028cc2160390a10846ff99cc96e1bd93a
Tyler Dansby <tdansby7@gmail.com>
2022-03-11 01:27:38 -0600

    svg animation progress

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,29 @@
 import './Header.css';
 import Logo from '../../utils/Logo.png';
+import {useState} from 'react';
 
 function Header(){
 
+    const [sideLinks,setSideLinks] = useState('closed');
     const handleSideLinks = () =>{
-        document.getElementById('lineOne')!.style.animation='lineOneMovement 2s linear';
-        document.getElementById('lineTwo')!.style.animation='lineTwoMovement 2s linear';
-        document.getElementById('lineThree')!.style.animation='lineThreeMovement 2s linear';
+        if(sideLinks==='closed'){
+            document.getElementById('lineOne')!.style.animation='lineOneMovement 0.5s forwards';
+            document.getElementById('lineTwo')!.style.animation='lineTwoMovement 0.5s forwards';
+            document.getElementById('lineThree')!.style.animation='lineThreeMovement 0.5s forwards';
+            setSideLinks('open');
+        }
+        else if(sideLinks==='open'){
+            document.getElementById('lineOne')!.style.animation='lineOneMovementRev 0.5s forwards';
+            document.getElementById('lineTwo')!.style.animation='lineTwoMovementRev 0.5s forwards';
+            document.getElementById('lineThree')!.style.animation='lineThreeMovementRev 0.5s forwards';
+            setSideLinks('closed');
+        }
     }
 
     return(
         <div className="header">
             <div id="logoAndResume">
-                <a id="logo" href="localhost:3000"><img alt='T logo' src={Logo} width='50px'></img></a>
+                <a id="logo" onClick={()=>{window.location.reload()}}><img alt='T logo' src={Logo} width='50px'></img></a>
                 <a id="resumeLink">Resume</a>
             </div>
             <div></div>
@@ -24,9 +35,9 @@ function Header(){
                 <a href='#contact'>Contact</a>
             </div>
                 <svg id="openSideLinks" onClick={()=>{handleSideLinks()}}>
-                    <line id="lineOne" x1="10" y1="11" x2="40" y2="11" stroke="black" strokeWidth='3'/>
-                    <line id="lineTwo" x1="10" y1="22" x2="40" y2="22" stroke="black" strokeWidth='3'/>
-                    <line id="lineThree" x1="10" y1="33" x2="40" y2="33" stroke="black" strokeWidth='3'/>
+                    <line id="lineOne" x1="5" y1="15" x2="45" y2="15" stroke="black" strokeWidth='3'/>
+                    <line id="lineTwo" x1="5" y1="25" x2="45" y2="25" stroke="black" strokeWidth='3'/>
+                    <line id="lineThree" x1="5" y1="35" x2="45" y2="35" stroke="black" strokeWidth='3'/>
                 </svg>
             
         </div>

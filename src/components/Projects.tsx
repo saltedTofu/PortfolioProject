0dee0f4ae0f085648a487e31f3c6d8d78860c2b6
Tyler Dansby <tdansby7@gmail.com>
2023-04-30 14:38:32 -0700

    added music metadata generator project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,7 @@ import Pomodoro from '../utils/Pomodoro.png';
 import LoLMatchMaker from '../utils/LoLMatchMaker.png'
 import Callouts from '../utils/Callouts.png';
 import ClickChamp from '../utils/ClickChamp.png';
+import MusicMeta from '../utils/MusicMeta.png'
 import {useState, useEffect} from 'react';
 import './styles/Projects.css';
 
@@ -32,6 +33,15 @@ function Projects(){
                 description='Dietitian Calc is a website designed for use by Clinical Dietitians to perform calculations for tube feedings, nutrition needs (kcal/protein), parenteral nutrition, etc,. The app features an area where the user can add and remove whichever calculators they need and drag them around the area. This app features user authentication and a database hosted on Firebase, and a web scraping tool built in Python for getting up to date tube feeding information.'
               />
             </div>
+          <div>
+            <Project 
+                name='Music Metadata Generator' 
+                gitLink="https://github.com/saltedTofu/youtube-to-mp3-metadata-fixer"
+                tech={['Python','OpenAI GPT3']}
+                img={MusicMeta}
+                description='Desktop app created for fixing metadata for .mp3 files made from Youtube videos using OpenAI. This script will iterate through a folder of .mp3 files, use GPT3 to find an appropriate title and artist, update the metadata for the file, and then rename the file to match the title. The user only needs to point to the folder of .mp3 files they want to generate the metadata for and then provide their OpenAI API key.'
+            />
+          </div>
           <div>
               <Project 
               name='PomoCustom' 
@@ -69,10 +79,8 @@ function Projects(){
                 tech={['React','CSS']}
                 img={Callouts}
                 description='This app is a tool for people who are trying to learn callout locations for the various competitive maps in Halo 3. Users will navigate to their desired map blueprint and mouse over sections of the map to show the name of the callout and an in-game screenshot on the right side panel. Users can use this app to learn callouts, to quiz themselves, or to use as a reference while playing Halo 3.'
-                />
+            />
           </div>
-            
-            
         </div>
       </div>
     )
